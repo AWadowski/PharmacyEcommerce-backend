14de27d2248b981f4d241d7c2d185dd5d77ebc46
Adam Wadowski <adam.wadowski@assecods.pl>
2023-05-30 10:14:38 +0200

    poprawa controllera

--- apteka-application/src/main/java/com/example/code/Controller/ProductController.java
+++ apteka-application/src/main/java/com/example/code/Controller/ProductController.java
@@ -64,7 +64,7 @@ public class ProductController {
     }
 
     @GetMapping("/typProduktu/{typProduktu}")
-    public List<ProductApi> getProductsByTypProduktu(@PathVariable String typProduktu) {
+    public List<ProductApi> getProductsByTypProduktu(@RequestBody TypProduktu typProduktu) {
         return productService.findProductsByTypProduktu(typProduktu);
     }
 
