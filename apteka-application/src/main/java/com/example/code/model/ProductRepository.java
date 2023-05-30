2956d33724eead779c341b7b048866ad24713078
Adam Wadowski <adam.wadowski@assecods.pl>
2023-05-30 10:43:28 +0200

    poprawa controllera

--- apteka-application/src/main/java/com/example/code/model/ProductRepository.java
+++ apteka-application/src/main/java/com/example/code/model/ProductRepository.java
@@ -17,22 +17,22 @@ import java.util.Optional;
 public interface ProductRepository extends CrudRepository<Product, Long>, JpaRepository<Product, Long> {
 
 
-    @Query("select id, name, price, description, typ, ingredients, picture_id from PRODUCT where id = :id")
+    @Query("SELECT p FROM PRODUCT p where p.id = :id")
     Optional<Product> findByid(Long id);
 
-    @Query("SELECT id, name, price, description, typ, ingredients, picture_id FROM PRODUCT WHERE description IS NOT NULL")
+    @Query("SELECT p FROM PRODUCT p WHERE p.description IS NOT NULL")
     List<Product> findProductsByDescriptionNotNull();
 
-    @Query("SELECT id, name, price, description, typ, ingredients, picture_id FROM PRODUCT WHERE price BETWEEN :minPrice AND :maxPrice")
+    @Query("SELECT p FROM PRODUCT p WHERE p.price BETWEEN :minPrice AND :maxPrice")
     List<Product> findProductsByPriceRange(double minPrice, double maxPrice);
 
-    @Query("SELECT id, name, price, description, typ, ingredients, picture_id FROM PRODUCT WHERE name = :name")
+    @Query("SELECT p FROM PRODUCT p WHERE p.name = :name")
     List<Product> findProductsByName(String name);
 
-    @Query("SELECT id, name, price, description, typ, ingredients, picture_id FROM PRODUCT WHERE price >= :price")
+    @Query("SELECT p FROM PRODUCT p WHERE p.price >= :price")
     List<Product> findProductsByPriceGreaterThanOrEqual(double price);
 
-    @Query("SELECT id, name, price, description, typ, ingredients, picture_id FROM PRODUCT WHERE name LIKE %:name%")
+    @Query("SELECT p FROM PRODUCT p WHERE p.name LIKE %:name%")
     List<Product> findProductsByNameContaining(String name);
 
     @Query("SELECT p FROM PRODUCT p WHERE p.typ = :typProduktu")
